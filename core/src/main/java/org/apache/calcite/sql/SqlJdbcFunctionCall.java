053b790ca62d95e3f161ca79d83f2e1f3cce4ba1
Sergei_Tsvetkov <9326077@gmail.com>
2018-07-12 14:27:00 +0300

    Support synonym for CENTURY

--- core/src/main/java/org/apache/calcite/sql/SqlJdbcFunctionCall.java
+++ core/src/main/java/org/apache/calcite/sql/SqlJdbcFunctionCall.java
@@ -708,6 +708,7 @@ public class SqlJdbcFunctionCall extends SqlFunction {
                   operands[0]);
             }
           });
+      map.put("CENTURY", simple(SqlStdOperatorTable.CENTURY));
       map.put("YEAR", simple(SqlStdOperatorTable.YEAR));
       map.put("QUARTER", simple(SqlStdOperatorTable.QUARTER));
       map.put("MONTH", simple(SqlStdOperatorTable.MONTH));
